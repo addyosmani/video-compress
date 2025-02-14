32130bc5657fe4a9b7797aabceb63a0516422406
Hung Doan <hungdoansy@gmail.com>
2025-02-14 08:18:50 +0700

    Make app work offline

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
+
 import App from './App.tsx';
 import './index.css';
+
+// prompt for a refresh when a new version is available
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
